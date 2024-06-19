09b38f3492e2d7bcc413e98c9b513fd9d89d412e
gtompel <quimpik@yandex.ru>
2024-06-19 16:17:01 +0300

    appwrite

---
+++ taskwrite/src/App.tsx
@@ -0,0 +1,8 @@
+import { client } from "./utils/appwrite";
+
+const App = () => {
+	console.log("Appwrite", client);
+	return <div className="text-purple-500 text-center font-bold text-	  5xl">App</div>;
+};
+
+export default App;
